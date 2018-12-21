b6136ca75a5787b12535c11e0b6265020e6445e2
backaligth <antoine.soros@orange.fr>
2018-12-21 11:02:07 +0100

    refactoring

--- src/app/meteo-pollution/shared/services/location-iq.service.ts
+++ src/app/meteo-pollution/shared/services/location-iq.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {token} from 'src/environments/environment';
+import {LocationIQ} from '../models/location-iq.models';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
@@ -11,8 +13,8 @@ export class LocationIqService {
   constructor(private http: HttpClient) {
 
   }
-      get(position: Position) {
-      return this.http.get(`https://eu1.locationiq.com/v1/reverse.php?key=${token.apiTokenLocationIq}
+      get(position: Position): Observable<LocationIQ> {
+      return this.http.get<LocationIQ>(`https://eu1.locationiq.com/v1/reverse.php?key=${token.apiTokenLocationIq}
       &lat=${position.coords.latitude}&lon=${position.coords.longitude}&tag=POI&radius=IN_METERS&format=json`);
   }
 }
