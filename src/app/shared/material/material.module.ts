dfda0357b646208942e260a58d04c19f98d7b9d8
backaligth <antoine.soros@orange.fr>
2018-12-23 23:29:49 +0100

    title larger size font

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -9,6 +9,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
+
 @NgModule({
   declarations: [],
   imports: [
